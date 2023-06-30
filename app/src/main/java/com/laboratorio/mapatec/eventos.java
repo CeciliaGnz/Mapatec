47614f46355c6a0eca87d3d05b1c4dfd385afe59
Cecilia González <ceciliasoto200309@gmail.com>
2023-06-30 10:45:59 -0500

    botton regresa para evento y perfil que lleva a inicio

--- app/src/main/java/com/laboratorio/mapatec/eventos.java
+++ app/src/main/java/com/laboratorio/mapatec/eventos.java
@@ -1,6 +1,7 @@
 package com.laboratorio.mapatec;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -8,15 +9,25 @@ import androidx.fragment.app.FragmentManager;
 
 public class eventos extends AppCompatActivity {
 
-    inicioFragment InicioFragment= new inicioFragment();
+    //inicioFragment InicioFragment= new inicioFragment();
+    Button btn_back;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events);
+
+        btn_back=(Button) findViewById(R.id.button_back);
     }
 
     public void Back(){
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, InicioFragment).commit();
+        inicioFragment InicioFragment= new inicioFragment();
+
+        // Obtener el FragmentManager
+        androidx.fragment.app.FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Reemplazar el contenido actual del contenedor del fragmento con el nuevo fragmento
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, InicioFragment)
+                .commit();
     }
 }
