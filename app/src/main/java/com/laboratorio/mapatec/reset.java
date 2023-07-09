62bd1ae422df42f6d4ba9ce91dc739a387eb7ba0
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-09 18:34:34 -0500

    update

--- app/src/main/java/com/laboratorio/mapatec/reset.java
+++ app/src/main/java/com/laboratorio/mapatec/reset.java
@@ -1,2 +1,18 @@
-package com.laboratorio.mapatec;public class reset {
+package com.laboratorio.mapatec;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class reset extends AppCompatActivity {
+
+    Button btn_si, btn_no;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_passw);
+
+
+    }
 }
