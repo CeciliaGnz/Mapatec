faf5b03e21a5ff9eef46bc6b3893b40495edd52e
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-10 10:52:49 -0500

    update

--- app/src/main/java/com/laboratorio/mapatec/Admin_activity.java
+++ app/src/main/java/com/laboratorio/mapatec/Admin_activity.java
@@ -8,13 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Admin_activity extends AppCompatActivity {
 
     TextView cedula_view;
-    Strind cedula;
+    String cedula;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seccion_admin);
 
         cedula_view=findViewById(R.id.cedu_adm);
 
+        Bundle extras = getIntent().getExtras();
         cedula = extras.getString("cedula");
         cedula_view.setText(cedula);
 
