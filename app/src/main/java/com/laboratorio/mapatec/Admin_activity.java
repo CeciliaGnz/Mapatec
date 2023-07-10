a318a46f727ddba365ff1ee1f6ca219d9cb231a0
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-10 10:23:59 -0500

    update

--- app/src/main/java/com/laboratorio/mapatec/Admin_activity.java
+++ app/src/main/java/com/laboratorio/mapatec/Admin_activity.java
@@ -1,4 +1,14 @@
 package com.laboratorio.mapatec;
 
-public class Admin_activity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Admin_activity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seccion_admin);
+
+    }
 }
