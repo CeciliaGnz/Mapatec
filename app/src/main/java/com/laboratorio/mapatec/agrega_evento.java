6f7a41e508d469d8c2faba1feb1bd90d70c05f80
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-15 11:35:55 -0500

    xml agregar

--- app/src/main/java/com/laboratorio/mapatec/agrega_evento.java
+++ app/src/main/java/com/laboratorio/mapatec/agrega_evento.java
@@ -1,8 +1,19 @@
 package com.laboratorio.mapatec;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class agrega_evento extends AppCompatActivity {
 
+    private EditText editTextHour;
+    private EditText editTextPlace;
+    private EditText editTextDescription;
+    private DatabaseHelper databaseHelper;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agregar_event);
+
+    }
 }
