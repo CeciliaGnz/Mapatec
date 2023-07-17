b955bc55dd24e1d08d44857e5ce0dce39c747795
Iesu16 <136399963+Iesu16@users.noreply.github.com>
2023-07-16 23:33:48 -0500

    hay google map

--- app/src/main/java/com/laboratorio/mapatec/mapaFragment.java
+++ app/src/main/java/com/laboratorio/mapatec/mapaFragment.java
@@ -1,76 +1,102 @@
 package com.laboratorio.mapatec;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import com.indooratlas.android.sdk.IALocationManager;
-import com.indooratlas.android.sdk.IALocationListener;
-import com.indooratlas.android.sdk.IALocation;
-import com.indooratlas.android.sdk.IALocationRequest;
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link mapaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class mapaFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-    private IALocationManager mIALocationManager;
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.IARegion.Listener;
 
-    public mapaFragment() {
-        // Required empty public constructor
-    }
+public class mapaFragment extends Fragment implements IALocationListener, Listener {
 
-    public static mapaFragment newInstance() {
-        return new mapaFragment();
-    }
+    private IALocationManager mLocationManager;
+    private TextView mLog;
+    private ScrollView mScrollView;
+    private long mRequestStartTime;
+
+    private static final String FASTEST_INTERVAL = "fastestInterval";
+    private static final String SHORTEST_DISPLACEMENT = "shortestDisplacement";
+
+    private long mFastestInterval = -1L;
+    private float mShortestDisplacement = -1f;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Configurar cualquier parámetro o argumento necesario
+        if (savedInstanceState != null) {
+            mFastestInterval = savedInstanceState.getLong(FASTEST_INTERVAL);
+            mShortestDisplacement = savedInstanceState.getFloat(SHORTEST_DISPLACEMENT);
+        }
 
-        // Inicializar IALocationManager
-        mIALocationManager = IALocationManager.create(requireContext());
+        // Inicializar el administrador de ubicación de IndoorAtlas
+        mLocationManager = IALocationManager.create(requireContext());
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_mapa, container, false);
-        // Inflar el diseño del fragmento y realizar cualquier configuración adicional
 
-        // Configurar IALocationListener para recibir actualizaciones de ubicación
-        IALocationListener mIALocationListener = new IALocationListener() {
-            @Override
-            public void onLocationChanged(IALocation iaLocation) {
-                // Lógica para manejar las actualizaciones de ubicación
-                double latitude = iaLocation.getLatitude();
-                double longitude = iaLocation.getLongitude();
-                // ...
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-                // Lógica para manejar los cambios de estado de ubicación
-            }
-        };
-
-        // Solicitar actualizaciones de ubicación
-        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mIALocationListener);
+        // Configurar vistas y otras inicializaciones
 
         return rootView;
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mLocationManager.requestLocationUpdates(IALocationRequest.create(), this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mLocationManager.removeLocationUpdates(this);
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
+        mLocationManager.destroy();
+    }
 
-        // Detener las actualizaciones de ubicación y liberar recursos
-        mIALocationManager.destroy();
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(FASTEST_INTERVAL, mFastestInterval);
+        outState.putFloat(SHORTEST_DISPLACEMENT, mShortestDisplacement);
+    }
+
+    // Implementar métodos de IALocationListener y Listener según sea necesario
+
+    @Override
+    public void onLocationChanged(IALocation iaLocation) {
+        // Lógica para manejar las actualizaciones de ubicación
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // Lógica para manejar los cambios de estado de ubicación
+    }
+
+    @Override
+    public void onEnterRegion(IARegion iaRegion) {
+        // Lógica para manejar la entrada a una región de IndoorAtlas
+    }
+
+    @Override
+    public void onExitRegion(IARegion iaRegion) {
+        // Lógica para manejar la salida de una región de IndoorAtlas
     }
 }
