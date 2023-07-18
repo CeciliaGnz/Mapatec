37f6ca2d1cabf76f07cacc593ddd0734603e3a38
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-18 17:14:12 -0500

    agregando lista

---
+++ app/src/main/java/com/laboratorio/mapatec/EventoAdapter.java
@@ -0,0 +1,32 @@
+package com.laboratorio.mapatec;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class EventoAdapter extends RecyclerView.Adapter {
+    public EventoAdapter(List<Evento> eventoList) {
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
