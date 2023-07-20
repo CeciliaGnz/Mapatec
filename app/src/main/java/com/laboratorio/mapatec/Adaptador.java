a870058d3672a56f9cd1491c88c5b3db4d5f0944
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-20 17:47:58 -0500

    restaurando cambios

--- app/src/main/java/com/laboratorio/mapatec/Adaptador.java
+++ app/src/main/java/com/laboratorio/mapatec/Adaptador.java
@@ -57,124 +57,105 @@ public class Adaptador extends BaseAdapter {
     @Override
     public View getView(int posicion, View view, ViewGroup viewGroup) {
         View vi=view;
-        if (vi == null) {
-            LayoutInflater li = (LayoutInflater) a.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            // Utiliza el diseño item_event.xml solo si estás en la actividad "eventos"
-            if (a instanceof eventos) {
-                vi = li.inflate(R.layout.item_event, null);
-            } else {
-                vi = li.inflate(R.layout.item, null);
-            }
+        if(vi==null){
+            LayoutInflater li= (LayoutInflater) a.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vi=li.inflate(R.layout.item,null );
         }
 
         v=lista.get(posicion);
 
-        // Si estás en la actividad "eventos", establece el contenido del TextView textViewEventoInfo
-        if (a instanceof eventos) {
-            TextView textViewEventoInfo = vi.findViewById(R.id.textViewEventoInfo);
 
-            // Concatenar los detalles del evento en una sola cadena de texto
-            String eventoInfo = "Título: " + v.getTitulo() + "\n"
-                    + "Fecha y hora: " + v.getFechaHora() + "\n"
-                    + "Lugar: " + v.getLugar() + "\n"
-                    + "Descripción: " + v.getDescripcion();
+        TextView id=(TextView) vi.findViewById(R.id.view_id);
+        TextView title=(TextView) vi.findViewById(R.id.view_title);
+        Button edit=(Button)vi.findViewById(R.id.edit);
+        Button delete=(Button)vi.findViewById(R.id.delete);
 
-            // Establecer la cadena de texto en el TextView
-            textViewEventoInfo.setText(eventoInfo);
-        } else {
-            TextView id = (TextView) vi.findViewById(R.id.view_id);
-            TextView title = (TextView) vi.findViewById(R.id.view_title);
-            Button edit = (Button) vi.findViewById(R.id.edit);
-            Button delete = (Button) vi.findViewById(R.id.delete);
+        // Convert the ID to string and set it as text for the TextView
+        id.setText(String.valueOf(v.getId()));
 
-            // Convert the ID to string and set it as text for the TextView
-            id.setText(String.valueOf(v.getId()));
+        title.setText(v.getTitulo());
+        edit.setTag(posicion);
+        delete.setTag(posicion);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //agregarevetno.xml
+                int pos=Integer.parseInt(view.getTag().toString());
+                Dialog dialogo=new Dialog(a);
+                dialogo.setTitle("Editar registro");
+                dialogo.setCancelable(true);
+                dialogo.setContentView(R.layout.agregar_event);
+                dialogo.show();
+                // Obtener referencias a los elementos de la interfaz de usuario
+                final EditText Titulo =(EditText)dialogo.findViewById(R.id.editTextTitulo);
+                final EditText FechaHora = (EditText)dialogo.findViewById(R.id.editTextHora);
+                final EditText Lugar = (EditText)dialogo.findViewById(R.id.editTextLugar);
+                final EditText Descripcion = (EditText)dialogo.findViewById(R.id.editTextDescripcion);
+                final Button btnAgregar = (Button)dialogo.findViewById(R.id.buttonAgregar);
+                btnAgregar.setText("Actualizar");
+                final Button btnCancelar = (Button)dialogo.findViewById(R.id.buttonCancelar);
 
-            title.setText(v.getTitulo());
-            edit.setTag(posicion);
-            delete.setTag(posicion);
-            edit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //agregarevetno.xml
-                    int pos = Integer.parseInt(view.getTag().toString());
-                    Dialog dialogo = new Dialog(a);
-                    dialogo.setTitle("Editar registro");
-                    dialogo.setCancelable(true);
-                    dialogo.setContentView(R.layout.agregar_event);
-                    dialogo.show();
-                    // Obtener referencias a los elementos de la interfaz de usuario
-                    final EditText Titulo = (EditText) dialogo.findViewById(R.id.editTextTitulo);
-                    final EditText FechaHora = (EditText) dialogo.findViewById(R.id.editTextHora);
-                    final EditText Lugar = (EditText) dialogo.findViewById(R.id.editTextLugar);
-                    final EditText Descripcion = (EditText) dialogo.findViewById(R.id.editTextDescripcion);
-                    final Button btnAgregar = (Button) dialogo.findViewById(R.id.buttonAgregar);
-                    btnAgregar.setText("Actualizar");
-                    final Button btnCancelar = (Button) dialogo.findViewById(R.id.buttonCancelar);
+                v=lista.get(pos);
+                setId(v.getId());
+                Titulo.setText(v.getTitulo());
+                FechaHora.setText(v.getFechaHora());
+                Lugar.setText(v.getLugar());
+                Descripcion.setText(v.getDescripcion());
 
-                    v = lista.get(pos);
-                    setId(v.getId());
-                    Titulo.setText(v.getTitulo());
-                    FechaHora.setText(v.getFechaHora());
-                    Lugar.setText(v.getLugar());
-                    Descripcion.setText(v.getDescripcion());
-
-                    btnAgregar.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            try {
-                                v = new Evento(getId(), Titulo.getText().toString(),
-                                        FechaHora.getText().toString(),
-                                        Lugar.getText().toString(),
-                                        Descripcion.getText().toString());
-                                dao.editar(v);
-                                lista = dao.verTodos();
-                                notifyDataSetChanged();
-                                dialogo.dismiss();
-                            } catch (Exception e) {
-                                Toast.makeText(a, "ERROR", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                    btnCancelar.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            dialogo.dismiss();
-                        }
-                    });
-                }
-            });
-
-            delete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //dialogo para confimar si/no eliminar
-                    int pos = Integer.parseInt(view.getTag().toString());
-                    v = lista.get(pos);
-                    setId(v.getId());
-                    AlertDialog.Builder del = new AlertDialog.Builder(a);
-                    del.setMessage("¿Estas seguro de eliminar el evento?");
-                    del.setCancelable(false);
-                    del.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dao.eliminar(getId());
-                            lista = dao.verTodos();
+                btnAgregar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            v=new Evento(getId(),Titulo.getText().toString(),
+                                    FechaHora.getText().toString(),
+                                    Lugar.getText().toString(),
+                                    Descripcion.getText().toString());
+                            dao.editar(v);
+                            lista=dao.verTodos();
                             notifyDataSetChanged();
+                            dialogo.dismiss();
+                        }catch (Exception e){
+                            Toast.makeText(a,"ERROR",Toast.LENGTH_SHORT).show();
                         }
-                    });
-                    del.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                btnCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialogo.dismiss();
+                    }
+                });
+            }
+        });
 
-                        }
-                    });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dialogo para confimar si/no eliminar
+                int pos=Integer.parseInt(view.getTag().toString());
+                v=lista.get(pos);
+                setId(v.getId());
+                AlertDialog.Builder del=new AlertDialog.Builder(a);
+                del.setMessage("¿Estas seguro de eliminar el evento?");
+                del.setCancelable(false);
+                del.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dao.eliminar(getId());
+                        lista=dao.verTodos();
+                        notifyDataSetChanged();
+                    }
+                });
+                del.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                    del.show();
-                }
-            });
+                    }
+                });
 
-        }
+                del.show();
+            }
+        });
 
         return vi;
     }
--- app/src/main/java/com/laboratorio/mapatec/eventos.java
+++ app/src/main/java/com/laboratorio/mapatec/eventos.java
@@ -4,17 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class eventos extends AppCompatActivity {
     Button btn_back;
-    private ArrayList<Evento> lista;
-    private ListView listViewEventos;
-    private Adaptador eventosAdapter;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +17,8 @@ public class eventos extends AppCompatActivity {
 
         btn_back=(Button) findViewById(R.id.button_back);
 
-        // Obtener la lista de eventos desde la base de datos o cualquier otra fuente de datos
-        // En este ejemplo, suponemos que ya tienes una lista de eventos (listaEventos)
 
-        // Inicializar el ListView y el adaptador
-        listViewEventos = findViewById(R.id.listViewEventos);
-        eventosAdapter = new Adaptador(this, lista);
 
-        // Asignar el adaptador al ListView
-        listViewEventos.setAdapter(eventosAdapter);
     }
 
     public void Back(View view){
