898c6d238d9836adc8d7f3ca447056a89a02b7ce
Cecilia González <ceciliasoto200309@gmail.com>
2023-07-19 21:13:05 -0500

    DIOS AYUDAME

--- app/src/main/java/com/laboratorio/mapatec/DatabaseHelper.java
+++ app/src/main/java/com/laboratorio/mapatec/DatabaseHelper.java
@@ -7,8 +7,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
